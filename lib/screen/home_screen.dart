9e6ff57a1d99213a3b61a0d07f96c3a5fc99024a
Clayton <wmjh0925@naver.com>
2023-09-17 17:53:04 +0900

    :sparkles: 페이지뷰 구현
    - 배경화면 리소스 확장자 및 네이밍 오타 수정 (png -> jpg, wallpaer -> wallpapaer)

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -6,7 +6,16 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Home Screen'),
+      body: PageView( // PageView 추가
+        children: [1, 2, 3, 4, 5, 6] // 샘플 리스트 생성
+            .map( // 위젯으로 매핑
+        (number) => Image.asset(
+            'assets/wallpapers/wallpaper_$number.jpg',
+            fit: BoxFit.cover, // BoxFit.cover 를 설정하면 이미지가 최대한 전체화면을 차지하도록 늘어남
+            ),
+          )
+        .toList(),
+      ),
     );
   }
 
