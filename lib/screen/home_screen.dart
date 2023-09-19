4165096d3c10dba4c3762d2e4b4496faf33d0868
Clayton <wmjh0925@naver.com>
2023-09-19 11:50:27 +0900

    :sparkles: 3초가 지나면 자동으로 페이지가 롤링 되도록 구현

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'dart:async';
 
 // StatefulWidget 정의
 class HomeScreen extends StatefulWidget {
@@ -11,12 +12,47 @@ class HomeScreen extends StatefulWidget {
 
 // HomeScreen의 State 정의
 class _HomeScreenState extends State<HomeScreen> {
+  final PageController pageController = PageController();
+  final int InitPageIndex = 0;
+  final int LastPageIndex = 5;
+
+  @override
+  void initState() {
+    super.initState();
+
+    Timer.periodic(
+      const Duration(seconds: 3),
+      (timer) {
+        int? pageIndex = pageController.page?.toInt(); // 현재 페이지 인덱스 가져오기
+
+        // 페이지 값이 없을 때 예외 처리
+        if (pageIndex == null) {
+          return;
+        }
+
+        // 마지막 페이지인 경우 분기 처리
+        if (pageIndex == LastPageIndex) {
+          pageIndex = InitPageIndex;
+        } else {
+          pageIndex++;
+        }
+
+        pageController.animateToPage(
+            pageIndex,
+            duration: const Duration(milliseconds: 500), // 애니메이션 연출 시간
+            curve: Curves.ease,
+        );
+      }
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     // 상태바 색상을 변경하기 위한 코드 (SystemUiOverlayStyle.dark 로 하면 검정색으로 설정 됨)
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
     return Scaffold(
       body: PageView( // PageView 추가
+        controller: pageController, // controller 등록
         children: [1, 2, 3, 4, 5, 6] // 샘플 리스트 생성
             .map( // 위젯으로 매핑
         (number) => Image.asset(
